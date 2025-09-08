a93cf081f1ba9ecaabe3ec5c4c7e60c7a7f4d1a8
rmfranken <robin.franken@epfl.ch>
2025-09-08 13:29:38 +0200

    refactor: use remote docker image

---
+++ tools/skohub-vocabs/src/components/header.jsx
@@ -0,0 +1,205 @@
+import React, { useEffect, useState } from "react"
+import { css } from "@emotion/react"
+import { Link, withPrefix } from "gatsby"
+import { getFilePath } from "../common"
+import { useSkoHubContext } from "../context/Context.jsx"
+import { getConfigAndConceptSchemes } from "../hooks/configAndConceptSchemes"
+import { getUserLang } from "../hooks/getUserLanguage"
+
+const Header = ({ siteTitle }) => {
+  const { config, conceptSchemes: conceptSchemesData } =
+    getConfigAndConceptSchemes()
+  const { data, updateState } = useSkoHubContext()
+  const style = css`
+    background: ${config.colors.skoHubWhite};
+
+    .headerContent {
+      padding: 20px 20px 0 20px;
+      display: flex;
+    }
+
+    .skohubLogo {
+      margin: 0;
+      display: inline-block;
+      width: calc(100% - 80px);
+
+      a {
+        text-decoration: none;
+        color: ${config.colors.skoHubDarkColor};
+      }
+
+      .skohubImg {
+        display: inline-block;
+        vertical-align: middle;
+        width: 30px;
+        height: 30px;
+      }
+
+      .skohubTitle {
+        display: inline-block;
+        vertical-align: middle;
+        padding: 0 0 0 15px;
+        font-size: 24px;
+        line-height: 24px;
+        font-weight: 700;
+
+        @media only screen and (max-width: 800px) {
+          padding: 0 0 0 8px;
+          font-size: 18px;
+        }
+      }
+      .conceptSchemes {
+        display: flex;
+      }
+
+      .conceptScheme {
+        padding: 15px 15px 0 0;
+        font-size: 24px;
+      }
+      .conceptScheme:not(:last-child):after {
+        content: ", ";
+      }
+    }
+
+    ul.language-menu {
+      margin: 0;
+      padding: 0;
+      list-style: none;
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
+      align-items: center;
+      width: auto;
+      min-width: 80px;
+
+      li {
+        margin: 0 0 0 10px;
+        display: inline-block;
+        padding: 0 2px;
+
+        button {
+          background: none;
+          display: inline-block;
+          padding: 5px;
+          color: ${config.colors.skoHubMiddleGrey};
+          border: 2px solid ${config.colors.skoHubMiddleGrey};
+
+          &:hover {
+            color: ${config.colors.skoHubAction};
+            border: 2px solid ${config.colors.skoHubAction};
+          }
+        }
+
+        .currentLanguage {
+          color: black;
+          font-weight: bold;
+          display: inline-block;
+          padding: 5px;
+          border: 2px solid ${config.colors.skoHubLightColor};
+        }
+      }
+    }
+  `
+  const [languages, setLanguages] = useState([])
+  const [language, setLanguage] = useState("")
+  const [title, setTitle] = useState("")
+
+  // set page language
+  useEffect(() => {
+    if (typeof languages !== "undefined" && languages.length) {
+      if (!data.selectedLanguage) {
+        const userLang = getUserLang({
+          availableLanguages: languages,
+        })
+        setLanguage(userLang)
+        // updateState({ ...data, selectedLanguage: userLang })
+      } else {
+        setLanguage(data.selectedLanguage)
+      }
+    }
+  }, [data])
+
+  // Set Languages
+  useEffect(() => {
+    if (!data?.currentScheme?.id) {
+      setLanguages(data.languages)
+    } else {
+      setLanguages(conceptSchemesData[data.currentScheme.id].languages)
+    }
+  }, [data])
+
+  // set title
+  useEffect(() => {
+    const title =
+      data.currentScheme?.title?.[data.selectedLanguage] ||
+      data.currentScheme?.prefLabel?.[data.selectedLanguage] ||
+      data.currentScheme?.dc_title?.[data.selectedLanguage] ||
+      data.currentScheme?.id
+    setTitle(title)
+  }, [data])
+
+  return (
+    <header css={style}>
+      <div className="headerContent">
+        <div className="skohubLogo">
+          <Link
+            to={`/`}
+            onClick={() => updateState({ ...data, currentScheme: {} })}
+          >
+            {config.logo && (
+              <img
+                className="skohubImg"
+                src={`${withPrefix("/img/" + config.logo)}`}
+                alt="SkoHub Logo"
+              />
+            )}
+            <span className="skohubTitle">{siteTitle}</span>
+          </Link>
+          {data?.currentScheme?.id && (
+            <div className="conceptSchemes">
+              <div
+                key={data.currentScheme.id}
+                className="conceptScheme"
+                onClick={() => {
+                  updateState({ ...data, currentScheme: data.currentScheme })
+                }}
+              >
+                <Link
+                  to={getFilePath(
+                    data.currentScheme.id,
+                    `html`,
+                    config.customDomain
+                  )}
+                >
+                  {title}
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
+        {languages && languages.length > 1 && (
+          <ul className="language-menu">
+            {languages.map((l) => (
+              <li key={l}>
+                {l === data.selectedLanguage ? (
+                  <button className="currentLanguage">{l}</button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      updateState({ ...data, selectedLanguage: l })
+                      setLanguage(l)
+                    }}
+                  >
+                    {l}
+                  </button>
+                )}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ tools/skohub-vocabs/src/styles/concepts.css.js
@@ -0,0 +1,205 @@
+import { css } from "@emotion/react"
+
+export const conceptStyle = (colors) => css`
+  display: flex;
+  height: 100%;
+
+  @media only screen and (max-width: 800px) {
+    display: block;
+    flex-direction: column;
+    height: auto;
+  }
+
+  a.current {
+    color: ${colors.skoHubMiddleColor};
+    font-weight: 700;
+  }
+
+  .block {
+    background-color: ${colors.skoHubWhite};
+    padding: 30px;
+    border-radius: 0px;
+    border: 2px solid ${colors.skoHubAction};
+
+    @media only screen and (max-width: 1024px) {
+      padding: 15px;
+    }
+  }
+
+  .concepts {
+    overflow: auto;
+  }
+
+  .nav-block {
+    flex: 1;
+
+    @media only screen and (max-width: 800px) {
+      display: block;
+      height: 45vh;
+      padding: 15px 15px 30px 15px;
+      width: 100%;
+    }
+  }
+
+  .main-block {
+    flex: 2;
+
+    @media only screen and (max-width: 800px) {
+      display: block;
+      width: 100%;
+    }
+  }
+
+  & > nav {
+    display: flex;
+    flex-direction: column;
+    overflow: auto;
+
+    & > ul {
+      overflow: auto;
+      margin: 0;
+      padding: 10px;
+      height: 100%;
+
+      @media only screen and (max-width: 800px) {
+        overflow: visible;
+        height: auto;
+      }
+    }
+
+    & > ul:before {
+      content: none;
+    }
+  }
+
+  .content {
+    position: relative;
+    overflow: auto;
+    margin-left: 20px;
+
+    @media only screen and (max-width: 800px) {
+      margin: 20px 0 0 0;
+    }
+
+    button {
+      padding: 10px 20px;
+    }
+
+    .json-png {
+      position: absolute;
+      right: 30px;
+      top: 30px;
+
+      @media only screen and (max-width: 1024px) {
+        position: relative;
+        left: 0;
+        right: 0;
+        top: 0;
+      }
+    }
+
+    h1 {
+      margin: 0;
+      padding: 0 0 15px 0;
+      font-size: 30px;
+      line-height: 32px;
+      font-weight: 700;
+
+      @media only screen and (max-width: 800px) {
+        font-size: 24px;
+        line-height: 26px;
+      }
+    }
+
+    h2 {
+      margin: 0;
+      padding: 0 0 0 0;
+      font-size: 24px;
+      line-height: 28px;
+      font-weight: 700;
+
+      @media only screen and (max-width: 800px) {
+        font-size: 20px;
+        line-height: 26px;
+      }
+    }
+
+    a {
+      color: ${colors.skoHubAction};
+      text-decoration: underline;
+
+      &:hover {
+        color: ${colors.skoHubMiddleColor};
+      }
+    }
+  }
+
+  .markdown {
+    padding-top: 10px;
+  }
+
+  .conceptURI {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    button {
+      border: 2px solid transparent;
+      border-radius: 5px;
+      padding: 5px;
+      margin: 5px;
+      background-color: transparent;
+      cursor: pointer;
+    }
+    button:hover {
+      border: 2px solid ${colors.skoHubMiddleColor};
+    }
+  }
+  /* Tooltip container */
+  .tooltip {
+    position: relative;
+    display: inline-block;
+  }
+
+  /* Tooltip text */
+  .tooltip .tooltiptext {
+    visibility: hidden;
+    width: 120px;
+    background-color: #555;
+    color: #fff;
+    text-align: center;
+    padding: 5px 0;
+    border-radius: 6px;
+
+    /* Position the tooltip text */
+    position: absolute;
+    z-index: 1;
+    bottom: 125%;
+    left: 50%;
+    margin-left: -60px;
+
+    /* Fade in tooltip */
+    opacity: 0;
+    transition: opacity 0.3s;
+  }
+
+  /* Tooltip arrow */
+  .tooltip .tooltiptext::after {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: #555 transparent transparent transparent;
+  }
+
+  /* Show the tooltip text when you mouse over the tooltip container */
+  .tooltip:hover .tooltiptext {
+    visibility: visible;
+    opacity: 1;
+  }
+`
+
+export default conceptStyle
